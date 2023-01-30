c00a0359cba25ddf53c3f1d313b2c3abc7514e4c
John-Dig <rohnfiggins@gmail.com>
2023-01-29 19:36:54 -0800

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import Triangle from "./triangle";
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
 function handleTriangleForm() {
   event.preventDefault();
+
   document.querySelector('#response').innerText = null;
   const length2 = parseInt(document.querySelector('#length2').value);
   const length1 = parseInt(document.querySelector('#length1').value);
