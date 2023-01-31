b6d1bd0e6955ecb778a87b0be64ddfa8dd2ed929
John-Dig <rohnfiggins@gmail.com>
2023-01-31 09:49:16 -0800

    good fail test 4

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -1,14 +1,14 @@
 import Triangle from './../src/triangle.js';
 
 describe('Triangle', () => {
-
+  //test 1
   test('should correctly create a triangle object with three lengths', () => {
     const triangle = new Triangle(2,4,5);
     expect(triangle.side1).toEqual(2);
     expect(triangle.side2).toEqual(4);
     expect(triangle.side3).toEqual(5);
   });
-
+  //test 2
   test('should correctly determine whether three lengths are not a triangle', () => {
     const notTriangle = new Triangle(3,9,22);
     expect(notTriangle.checkType()).toEqual("not a triangle");
@@ -18,6 +18,11 @@ describe('Triangle', () => {
   test('should correctly determine whether three lengths make a scalene triangle', () => {
     const scalTriangle = new Triangle(4,5,7)
     expect(scalTriangle.checkType()).toEqual("scalene triangle");
+  });
+  //test 4
+  test('should correctly determine whether three lengths make an isosceles triangle', () => {
+    const isoscTriangle = new Triangle(5,5,7)
+    expect(isoscTriangle.checkType()).toEqual("isosceles triangle");
   });
 });
 
